fdeb9e4b58bf7804dd16894e0379cfa8ebe9fc01
Kenan Sultanic <kenansultanic0805@gmail.com>
2023-08-24 12:14:25 +0200

    registered location model to admin panel

--- server/panel/admin.py
+++ server/panel/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Destination, Category, OpenTime, PublicTransportLine
+from .models import Destination, Category, OpenTime, PublicTransportLine, Location
 
 admin.site.register(Destination)
 admin.site.register(Category)
 admin.site.register(OpenTime)
 admin.site.register(PublicTransportLine)
+admin.site.register(Location)
