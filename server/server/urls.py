08c866afc4de13fe05bd63509b20dae5e3b9421e
Kenan Sultanic <kenansultanic0805@gmail.com>
2024-02-20 21:39:15 +0100

    molim te radi sad

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -19,10 +21,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('',include("panel.urls")),
+    path('', include("panel.urls")),
     #path('',include_docs_urls(title='Todo Api')),
     path('admin/', admin.site.urls),
     path('docs/', include_docs_urls(title='Todo Api')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
