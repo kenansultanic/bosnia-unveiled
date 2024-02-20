fed1e14b2a18ed036da8f0de150e6e39720ddf5c
Kenan Sultanic <kenansultanic0805@gmail.com>
2024-02-20 15:10:34 +0100

    updated wsgi

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -5,9 +5,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 application = get_wsgi_application()
 app = application
