af42d5547543bab1721475e757109f59389bf067
dpasalic <pasalic.dev@gmail.com>
2024-04-03 17:57:46 +0200

    added random destinations

---
+++ client/src/pages/Home/TopPicksSkeleton.tsx
@@ -0,0 +1,15 @@
+import "./home.scss";
+
+const TopPicksSkeleton = () => {
+    return (
+        <div className="top-picks-cards-skeleton">
+            <div className="top-picks-cards-container">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default TopPicksSkeleton;
