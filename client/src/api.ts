494798d4d5f0375aed1a561cbc2d2d7e50d8370e
kenansultanic <111312449+kenansultanic@users.noreply.github.com>
2025-07-27 13:50:23 +0200

    Updated server base URL

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://bosnia-unveiled.vercel.app/"
+    baseURL: "https://bosnia-unveiled-git-production-kenansultanics-projects.vercel.app/"
 });
